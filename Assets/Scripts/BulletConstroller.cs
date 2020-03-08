d38885291e9ff258cf4a72f7eec51fe9058137f0
giaosame <qzcqs@icloud.com>
2020-03-08 15:43:44 -0400

    add base code

---
+++ Assets/Scripts/BulletConstroller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletConstroller : MonoBehaviour
+{
+	private Transform bullet;
+	public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullet = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bullet.position += Vector3.up * speed;
+
+        if (bullet.position.y >= 10)
+        	Destroy (gameObject);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+            
+            // Increases player's score
+            // PlayerScore.playerScore += 100;
+            // Audio.isInvaderKilled = true;
+        }
+        else if(other.CompareTag("Base"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerController : MonoBehaviour
+{
+	private Transform player;
+	public float speed;
+	public float maxBound = 10;
+	public float minBound = -10;
+
+	public GameObject bullet;
+	public Transform spawnBullet;
+	public float shotRate;
+
+	private float nextShot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	player = GetComponent<Transform>();    
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        if (player.position.x < minBound && h < 0)
+        	h = 0;
+        else if (player.position.x > maxBound && h > 0)
+        	h = 0;
+
+        player.position += Vector3.right * h * speed;
+    }
+
+    void Update()
+    {
+    	if (Input.GetButton("Fire1") && Time.time > nextShot)
+    	{
+    		print("Hello World!");
+    		nextShot = Time.time + shotRate;
+    		Instantiate(bullet, spawnBullet.position, spawnBullet.rotation);
+    	}
+    }
+}
